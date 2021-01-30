2c6393eb2ab6e4e12450d9c9647764822588c515
kevans054 <kjevans@telus.net>
2021-01-30 13:43:23 -0700

    my first python script

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+nums = [3, 6, 43, 50]
+
+
+
+def process(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
+
+for num in nums:
+    process(num)
